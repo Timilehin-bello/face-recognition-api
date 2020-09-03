94a037382e59effb1c01d7d63b92675b8c4e9cd2
Bello Emmanuel Oluwatimilehin <Timilehin-bello@users.noreply.github.com>
2020-09-03 18:48:04 +0100

    First Commit

---
+++ controllers/image.js
@@ -0,0 +1,35 @@
+
+const Clarifai = require('clarifai');
+
+
+
+
+const app = new Clarifai.App({
+    apiKey: 'b986e98890be4fbfb40db9a2a92284c1'
+});
+
+const handleApiCall = (req, res) => {
+  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input) 
+  .then(data => {
+      res.json(data);
+  }) 
+  .catch(err => res.status(400).json('Unable to work with API'))
+}
+   
+
+const handleImage =(req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0])
+    })
+    .catch(err => res.status(400).json('Error getting entries'))
+};
+
+module.exports = {
+    handleImage,
+    handleApiCall
+
+}
