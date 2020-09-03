79d9feb1edc83c1000ff47926eea65ee56dfc364
Bello Emmanuel Oluwatimilehin <Timilehin-bello@users.noreply.github.com>
2020-09-03 20:58:15 +0100

    Adding Environment Variable PORT

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json())
 
 app.use(cors())
 
-app.get('/', (req, res) => {res.send(database.users)})
+app.get('/', (req, res) => {res.send('It is Working')})
 
 app.post('/signin', (req, res) =>{ signin.handleSignin(req, res, db, bcrypt)});
   
@@ -36,6 +36,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
 
-app.listen(3001, () => {
-    console.log('App is really running check it out')
+app.listen(process.env.PORT || 3001, () => {
+    console.log(`App is running check on port ${process.env.PORT}`)
 });
