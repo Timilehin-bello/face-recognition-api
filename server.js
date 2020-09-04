005d20d110f4260d9a88f43cca53ea41d3534a39
Bello Emmanuel Oluwatimilehin <Timilehin-bello@users.noreply.github.com>
2020-09-04 18:56:56 +0100

    Commit

--- server.js
+++ server.js
@@ -8,13 +8,15 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+
 const db = knex({
     client: 'pg',
     connection: {
         connectionString: 'process.env.DATABASE_URL',
-        ssl: true
+        ssl: true 
     }
 });
+
 
 const app = express();
 
