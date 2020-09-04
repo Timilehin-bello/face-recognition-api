c2d212d86602f34f601a9de89153d339ad82f2a3
Bello Emmanuel Oluwatimilehin <Timilehin-bello@users.noreply.github.com>
2020-09-04 01:29:40 +0100

    Connecting to postgres

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: '55596441',
-        database: 'face_recognition'
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
