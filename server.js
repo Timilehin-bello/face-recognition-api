344b37435ab0d54da44db1c99517a5d424ef7171
Bello Emmanuel Oluwatimilehin <Timilehin-bello@users.noreply.github.com>
2020-09-09 12:05:38 +0100

    fixing connection to heroku  postgres

--- server.js
+++ server.js
@@ -11,9 +11,12 @@ const image = require('./controllers/image');
 
 const db = knex({
     client: 'pg',
-    connection: {
-        connectionString: 'process.env.DATABASE_URL',
-        ssl: true
+    connection: process.env.DATABASE_URL || {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: true
     }
 });
 
